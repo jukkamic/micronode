b7ae51c8b403f40ef6408944405424118539ad9f
Varimo <varimo@iki.fi>
2023-02-12 14:43:39 +0200

    Added persistent cart

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask,request
 from flask_restful import Resource, Api
+import os
 
 app = Flask(__name__)
 api = Api(app)
@@ -10,17 +11,36 @@ class HelloWorld(Resource):
 
 api.add_resource(HelloWorld, '/')
 
+
 class Cart(Resource):
     def __init__(self):
+        self.filename = 'cart.txt'
         self.items = []
+        if os.path.exists(self.filename):
+            with open(self.filename, 'r') as f:
+                self.items = [eval(line.strip()) for line in f]
 
     def get(self):
-        return {'items': self.items}
+        return {'items': self.items, 'total': self.calculate_total()}
 
     def post(self):
         item = request.get_json()
         self.items.append(item)
-        return {'item': item}, 201
+        with open(self.filename, 'a') as f:
+            f.write(str(item) + '\n')
+        return {'item': item, 'total': self.calculate_total()}, 201
+
+    def delete(self):
+        self.items = []
+        open(self.filename, 'w').close()
+        return {'message': 'Cart is now empty'}
+
+    def calculate_total(self):
+        total = 0
+        for item in self.items:
+            total += item['price'] * item['quantity']
+        return total * 1.05  # Add 5% tax
+
 
 api.add_resource(Cart, '/cart')
 
--- test_cart.py
+++ test_cart.py
@@ -6,21 +6,38 @@ class CartTestCase(unittest.TestCase):
     def setUp(self):
         self.app = app.app
         self.client = self.app.test_client()
+        
+
+    def tearDown(self):
+        response = self.client.delete('/cart')
+        self.assertEqual(response.status_code, 200)
+        self.assertDictEqual(response.get_json(), {'message': 'Cart is now empty'})
+        self.app = None
+        self.client = None
 
     def test_get_empty_cart(self):
         response = self.client.get('/cart')
         self.assertEqual(response.status_code, 200)
-        self.assertDictEqual(response.get_json(), {'items': []})
+        self.assertDictEqual(response.get_json(), {'items': [], 'total': 0})
 
     def test_post_to_cart(self):
-        item = {'name': 'item1', 'price': 10}
+        item = {'name': 'item1', 'price': 10, 'quantity': 2}
         response = self.client.post('/cart', json=item)
         self.assertEqual(response.status_code, 201)
-        self.assertDictEqual(response.get_json(), {'item': item})
+        self.assertDictEqual(response.get_json(), {'item': item, 'total': 21})
+
+    def test_calculate_total(self):
+        item1 = {'name': 'item1', 'price': 10, 'quantity': 2}
+        item2 = {'name': 'item2', 'price': 5, 'quantity': 3}
+        response = self.client.post('/cart', json=item1)
+        self.assertEqual(response.status_code, 201)
+        response = self.client.get('/cart')
+        response = self.client.post('/cart', json=item2)
+        self.assertEqual(response.status_code, 201)
 
         response = self.client.get('/cart')
         self.assertEqual(response.status_code, 200)
-        self.assertDictEqual(response.get_json(), {'items': [item]})
+        self.assertDictEqual(response.get_json(), {'items': [item1, item2], 'total': 36.75})
 
 if __name__ == '__main__':
     unittest.main()
